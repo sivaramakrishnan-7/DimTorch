bf8faee4fea81cc47cf8fc345faf945b2c15b811
quantum-sage <65615807+quantum-sage@users.noreply.github.com>
2025-03-02 01:03:55 +0530

    added few more functions to operators.py

--- core/operators.py
+++ core/operators.py
@@ -14,7 +14,10 @@ def exp(a):
     return DimTensor(np.exp(a.tensor), grad=a.grad)
 
 def log(a):
-    return DimTensor(np.log(a.tensor), grad = a.grad)
+    if (a.tensor > 0).all():
+        return DimTensor(np.log(a.tensor), grad = a.grad)
+    else:
+        raise ValueError("Inputs must be positive")
 
 def sigmoid(a):
     sig =  1/(1 + np.exp(-a.tensor))
@@ -27,6 +30,17 @@ def is_close(a,b, tol =1e-5):
     b = a._to_tensor(b)
     return (np.abs(a.tensor - b.tensor) < tol).all()
 
+def inv(a):
+    return DimTensor(1/a.tensor, grad = a.grad)
+
+def log_back(a, grad):
+    return DimTensor(grad.tensor * (1/a.tensor), grad = a.grad)
+
+def inv_back(a,grad):
+    return DimTensor(grad.tensor * (-1/(a.tensor)**2), grad = a.grad)
+
+def relu_back(a,grad):
+    return DimTensor(grad.tensor*(a.tensor >0), grad=a.grad)
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from core.tensor import DimTensor
 from core.operators import exp,log,max,sigmoid,relu
 
-t1 = DimTensor([1, -3], True)
+t1 = DimTensor([1, 1], True)
 print(exp(t1))     # [2.718..., 0.049...]
 print(log(t1))     # [0, error—needs positive input]
 print(sigmoid(t1)) # [0.731..., 0.047...]
